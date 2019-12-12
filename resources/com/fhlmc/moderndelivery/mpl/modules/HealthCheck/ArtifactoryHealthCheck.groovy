331d0fa9da3549d76ce542561e918c4c313d4307
Bode Fuwa <bode@IftikhaddinsMBP.fios-router.home>
2019-12-12 09:54:23 -0500

    feature/fmt-41 added healthcheck modules

---
+++ resources/com/fhlmc/moderndelivery/mpl/modules/HealthCheck/ArtifactoryHealthCheck.groovy
@@ -0,0 +1,36 @@
+
+import com.fhlmc.moderndelivery.mpl.*
+
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+print 'Checking Artifactory Status....'
+
+HttpURLConnection urlConnection;
+def responseCode
+def targetUrl = 'https://pilot.agiletrailblazers.com/artifactory'
+
+try {
+    urlConnection = (HttpURLConnection) new URL(targetUrl).openConnection()
+    urlConnection.setRequestMethod("HEAD")
+    urlConnection.setConnectTimeout(2000)
+    urlConnection.setReadTimeout(2000)
+    responseCode = urlConnection.getResponseCode();
+
+} catch (Exception e) {
+    e.printStackTrace()
+    responseCode = 0
+}
+
+
+if(responseCode == 200)
+{
+    print 'OK'
+    println ''
+}
+else
+{
+    println responseCode + ' FAIL'
+    throw new MPLModuleException("Artifactory health check failed: Response code " + responseCode)
+}
--- resources/com/fhlmc/moderndelivery/mpl/modules/HealthCheck/HealthCheck.groovy
+++ resources/com/fhlmc/moderndelivery/mpl/modules/HealthCheck/HealthCheck.groovy
@@ -6,3 +6,8 @@ echo 'Toolchain Healthcheck ...'
 
 MPLModule('Jira HealthCheck', CFG)
 MPLModule('Sonar HealthCheck', CFG)
+MPLModule('Artifactory HealthCheck', CFG)
+MPLModule('Elasticache HealthCheck', CFG)
+MPLModule('Fluentd HealthCheck', CFG)
+MPLModule('Kibana HealthCheck', CFG)
+MPLModule('Spinnaker HealthCheck', CFG)
