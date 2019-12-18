2d9839d6385b7037151ed0b72cc31bd44983d76f
sasi kumar anandan <skumar@agiletrailblazers.com>
2019-12-18 18:36:56 +0000

    MavenBuild.groovy edited online with Bitbucket

--- resources/com/fhlmc/moderndelivery/mpl/modules/Build/MavenBuild.groovy
+++ resources/com/fhlmc/moderndelivery/mpl/modules/Build/MavenBuild.groovy
@@ -7,13 +7,9 @@ if (CFG.'maven.goals'){
 }
 /* Default build on Maven 3 */
 
-def str2 = "modp_jenkins_worker_1" 
-if (config.agent_label ==str2){
 withEnv(["PATH+MAVEN=${tool(CFG.'maven.tool_version' ?: 'Maven 3')}/bin"]) {
   def settings = CFG.'maven.settings_path' ? "-s '${CFG.'maven.settings_path'}'" : ''
   sh """mvn -B ${settings} -DargLine='-Xmx1024m -XX:MaxPermSize=1024m' ${mavenGoals}"""
-}
-
 }
 
 
