c7a422fdfb4dcdbb9984addb806accca8e072fab
sasi kumar anandan <skumar@agiletrailblazers.com>
2019-12-18 23:05:08 +0000

    SonarQube.groovy edited online with Bitbucket

--- resources/com/fhlmc/moderndelivery/mpl/modules/CodeScan/SonarQube.groovy
+++ resources/com/fhlmc/moderndelivery/mpl/modules/CodeScan/SonarQube.groovy
@@ -2,16 +2,12 @@
  * Simple Sonar Scan
  */
 
-
-withEnv(["PATH+GRADLE=${tool(CFG.'gradle.tool_version' ?: 'Gradle 5')}/bin"]) {
-  def settings = CFG.'gradle.settings_path' ? "-s '${CFG.'gradle.settings_path'}'" : ''
-  sh "gradle build"
-}
-
-
 //script {
-  //   def sonarqubeScannerHome = tool name: 'SonarQubeScanner'
-  //   withSonarQubeEnv('SonarQubeScanner') {
-   //       sh "${sonarqubeScannerHome}/bin/sonar-scanner --version"
-    //    }
+     def sonarqubeScannerHome = tool name: 'SonarQubeScanner'
+     withSonarQubeEnv('SonarQubeScanner') {
+          sh "${sonarqubeScannerHome}/bin/sonar-scanner --version"
+        }
  // }
+
+
+
