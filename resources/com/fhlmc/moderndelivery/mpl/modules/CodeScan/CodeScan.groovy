9f1e953816abc07978f01a07bab3b00757ae0e01
sasi kumar anandan <skumar@agiletrailblazers.com>
2019-12-18 22:59:12 +0000

    CodeScan.groovy edited online with Bitbucket

--- resources/com/fhlmc/moderndelivery/mpl/modules/CodeScan/CodeScan.groovy
+++ resources/com/fhlmc/moderndelivery/mpl/modules/CodeScan/CodeScan.groovy
@@ -2,13 +2,5 @@ if( CFG.'scan_type' ){
   MPLModule('Fortify Scan', CFG)
 }
 else{
- // MPLModule('Sonar Qube', CFG)
-  
-  script {
-     def sonarqubeScannerHome = tool name: 'SonarQubeScanner'
-     withSonarQubeEnv('SonarQubeScanner') {
-          sh "${sonarqubeScannerHome}/bin/sonar-scanner --version"
-        }
-  }
-  
+  MPLModule('Sonar Qube', CFG)
 }
