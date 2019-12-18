084297cf394f78ac04580bcde40ebfa356113fc3
sasi kumar anandan <skumar@agiletrailblazers.com>
2019-12-18 22:29:14 +0000

    CodeScan.groovy edited online with Bitbucket

--- resources/com/fhlmc/moderndelivery/mpl/modules/CodeScan/CodeScan.groovy
+++ resources/com/fhlmc/moderndelivery/mpl/modules/CodeScan/CodeScan.groovy
@@ -1,2 +1,5 @@
 
-MPLModule('Sonar Qube', CFG)
+//MPLModule('Sonar Qube', CFG)
+def scannerHome = tool 'SonarQubeScanner';
+     withSonarQubeEnv('SonarQubeScanner') { // If you have configured more than one global server connection, you can specify its name
+       sh "${scannerHome}/bin/sonar-scanner"
