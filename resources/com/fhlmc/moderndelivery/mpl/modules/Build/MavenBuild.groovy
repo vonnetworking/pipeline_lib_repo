4c02703231f10e631e8f4dbe0978055964d50bb4
sasi kumar anandan <skumar@agiletrailblazers.com>
2019-12-18 17:46:24 +0000

    MavenBuild.groovy edited online with Bitbucket

--- resources/com/fhlmc/moderndelivery/mpl/modules/Build/MavenBuild.groovy
+++ resources/com/fhlmc/moderndelivery/mpl/modules/Build/MavenBuild.groovy
@@ -6,22 +6,12 @@ if (CFG.'maven.goals'){
   mavenGoals = CFG.'maven.goals'
 }
 /* Default build on Maven 3 */
-/*withEnv(["PATH+MAVEN=${tool(CFG.'maven.tool_version' ?: 'Maven 3')}/bin"]) {
+withEnv(["PATH+MAVEN=${tool(CFG.'maven.tool_version' ?: '${CFG.'maven.Build.tool_version'}')}/bin"]) {
   def settings = CFG.'maven.settings_path' ? "-s '${CFG.'maven.settings_path'}'" : ''
   sh """mvn -B ${settings} -DargLine='-Xmx1024m -XX:MaxPermSize=1024m' ${mavenGoals}"""
-}
-*/
-if (CFG.'modules.Build.tool_version'='Maven 3'){
-withEnv(["PATH+MAVEN=${tool(CFG.'maven.tool_version' ?: 'Maven 3')}/bin"]) {
-  def settings = CFG.'maven.settings_path' ? "-s '${CFG.'maven.settings_path'}'" : ''
-  sh """mvn -B ${settings} -DargLine='-Xmx1024m -XX:MaxPermSize=1024m' ${mavenGoals}"""
-}
-  
-withEnv(["PATH+MAVEN=${tool(CFG.'maven.tool_version' ?: 'Maven 3.6')}/bin"]) {
-  def settings = CFG.'maven.settings_path' ? "-s '${CFG.'maven.settings_path'}'" : ''
-  sh """mvn -B ${settings} -DargLine='-Xmx1024m -XX:MaxPermSize=1024m' ${mavenGoals}"""
-}  
 }
 
 
 
+
+/*'${CFG.'maven.Build.tool_version'}' */
