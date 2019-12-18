f53aa278784f7f15be45ad403603758a85f43c8f
sasi kumar anandan <skumar@agiletrailblazers.com>
2019-12-18 22:30:48 +0000

    CodeScan.groovy edited online with Bitbucket

--- resources/com/fhlmc/moderndelivery/mpl/modules/CodeScan/CodeScan.groovy
+++ resources/com/fhlmc/moderndelivery/mpl/modules/CodeScan/CodeScan.groovy
@@ -3,3 +3,4 @@
 def scannerHome = tool 'SonarQubeScanner';
      withSonarQubeEnv('SonarQubeScanner') { // If you have configured more than one global server connection, you can specify its name
        sh "${scannerHome}/bin/sonar-scanner"
+     }
