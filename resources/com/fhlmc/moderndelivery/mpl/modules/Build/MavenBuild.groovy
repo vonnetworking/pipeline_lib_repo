d30ad0c625eecade8e222992901f2b900a22ae68
sasi kumar anandan <skumar@agiletrailblazers.com>
2019-12-18 18:42:41 +0000

    MavenBuild.groovy edited online with Bitbucket

--- resources/com/fhlmc/moderndelivery/mpl/modules/Build/MavenBuild.groovy
+++ resources/com/fhlmc/moderndelivery/mpl/modules/Build/MavenBuild.groovy
@@ -10,7 +10,7 @@ if (CFG.'maven.goals'){
 withEnv(["PATH+MAVEN=${tool(CFG.'maven.tool_version' ?: 'Maven 3')}/bin"]) {
   def settings = CFG.'maven.settings_path' ? "-s '${CFG.'maven.settings_path'}'" : ''
   sh """mvn -B ${settings} -DargLine='-Xmx1024m -XX:MaxPermSize=1024m' ${mavenGoals}"""
-  sh "which maven"
+  sh "mvn -v"
 }
 
 
