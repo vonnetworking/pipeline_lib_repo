624e6699dceff60afe96beb9f954396c968f6511
sasi kumar anandan <skumar@agiletrailblazers.com>
2019-12-19 21:21:19 +0000

    MavenBuild.groovy edited online with Bitbucket

--- resources/com/fhlmc/moderndelivery/mpl/modules/Build/MavenBuild.groovy
+++ resources/com/fhlmc/moderndelivery/mpl/modules/Build/MavenBuild.groovy
@@ -14,11 +14,17 @@ if (CFG.'maven_tool_version'){
 
 withEnv(["PATH+MAVEN=${tool(CFG.'maven.tool_version' ?: """${mavenVersion}""")}/bin"]) {
   def settings = CFG.'maven.settings_path' ? "-s '${CFG.'maven.settings_path'}'" : ''
-  sh "mvn -v"
-  sh """mvn -B ${settings} -DargLine='-Xmx1024m -XX:MaxPermSize=1024m' ${mavenGoals}""" 
-  junit 'target/*/*.xml'
+  sh "mvn -v"  
+  def exists = fileExists 'pom.xml'
+    if (exists) {
+        echo 'Yes pom.xml exists at root'
+      sh """mvn -B ${settings} -DargLine='-Xmx1024m -XX:MaxPermSize=1024m' ${mavenGoals}"""  
+      /* Publish Junit Test Results */
+      junit 'target/*/*.xml'
+    } else {
+        echo 'No pom.xml exists'
+    }
   
-  sh "cat target/surefire-reports/*.xml"
 }
 
 
