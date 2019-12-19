a3ed7be81291259d1c0a977e01e4ae3fadb651ec
sasi kumar anandan <skumar@agiletrailblazers.com>
2019-12-19 01:35:10 +0000

    MavenBuild.groovy edited online with Bitbucket

--- resources/com/fhlmc/moderndelivery/mpl/modules/Build/MavenBuild.groovy
+++ resources/com/fhlmc/moderndelivery/mpl/modules/Build/MavenBuild.groovy
@@ -17,7 +17,7 @@ withEnv(["PATH+MAVEN=${tool(CFG.'maven.tool_version' ?: """${mavenVersion}""")}/
   sh "mvn -v"
   sh """mvn -B ${settings} -DargLine='-Xmx1024m -XX:MaxPermSize=1024m' ${mavenGoals}""" 
   junit 'target/*/*.xml'
-  archiveArtifacts 'target/surefire-reports/*/*.xml'
+  
   sh "cat target/surefire-reports/*.xml"
 }
 
