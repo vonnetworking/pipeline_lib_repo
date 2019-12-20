385445502b0d650656b7fbf8ba0b9eb161292a5d
sasi kumar anandan <skumar@agiletrailblazers.com>
2019-12-20 02:06:24 +0000

    MavenBuild.groovy edited online with Bitbucket

--- resources/com/fhlmc/moderndelivery/mpl/modules/Build/MavenBuild.groovy
+++ resources/com/fhlmc/moderndelivery/mpl/modules/Build/MavenBuild.groovy
@@ -23,8 +23,11 @@ withEnv(["PATH+MAVEN=${tool(CFG.'maven.tool_version' ?: """${mavenVersion}""")}/
     if (exists) {
         echo 'Yes pom.xml exists at root'
       sh """mvn -B ${settings} -DargLine='-Xmx1024m -XX:MaxPermSize=1024m' ${mavenGoals}"""  
-      /* Publish Junit Test Results */
+      /* Publish Junit Test Results if the tests ran*/
+      def test_results_exist = fileExists 'target/*/*.xml'
+      if (test_results_exist){
       junit 'target/*/*.xml'
+      }
     } else {
         def newex = new MPLModuleException("Found error during execution. No pom.xml file exists in the project workspace")
       newex.setStackTrace(Helper.getModuleStack(newex))
