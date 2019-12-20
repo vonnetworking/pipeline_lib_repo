1cc0972eab8e8a2cfd9e29011ef317d34acbb6f8
Bode Fuwa <bode@IftikhaddinsMBP.fios-router.home>
2019-12-20 15:36:17 -0500

    adding unit tests

---
+++ test/groovy/com/fhlmc/moderndelivery/mpl/modules/Checkout/CheckoutTest.groovy
@@ -0,0 +1,41 @@
+//
+// Copyright (c) 2019 Agile Trailblazers, Inc. All Rights Reserved
+// https://www.agiletrailblazers.com
+//
+// Classification level: Confidential
+//
+// $Id: $
+// @Project:     MPL
+// @Description: Shared Jenkins Modular Pipeline Library
+//
+
+import org.junit.Before
+import org.junit.Test
+
+import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
+import static com.lesfurets.jenkins.unit.global.lib.LocalSource.localSource
+
+import static org.assertj.core.api.Assertions.assertThat
+
+import com.fhlmc.moderndelivery.mpl.Helper
+import com.fhlmc.moderndelivery.mpl.testing.MPLTestBase
+
+class CheckoutTest extends MPLTestBase {
+    def script = null
+
+    @Override
+    @Before
+    void setUp() throws Exception {
+        String sharedLibs = this.class.getResource('.').getFile()
+
+        helper.registerSharedLibrary(library()
+                .name('mpl')
+                .allowOverride(false)
+                .retriever(localSource(sharedLibs))
+                .targetPath(sharedLibs)
+                .defaultVersion('snapshot')
+                .implicit(true)
+                .checkout()
+        )
+    }
+}
---
+++ test/groovy/com/fhlmc/moderndelivery/mpl/modules/Test/TestTest.groovy
@@ -0,0 +1,25 @@
+//
+// Copyright (c) 2019 Agile Trailblazers, Inc. All Rights Reserved
+// https://www.agiletrailblazers.com
+//
+// Classification level: Confidential
+//
+// $Id: $
+// @Project:     MPL
+// @Description: Shared Jenkins Modular Pipeline Library
+//
+
+import org.junit.Before
+import org.junit.Test
+
+import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
+import static com.lesfurets.jenkins.unit.global.lib.LocalSource.localSource
+
+import static org.assertj.core.api.Assertions.assertThat
+
+import com.fhlmc.moderndelivery.mpl.Helper
+import com.fhlmc.moderndelivery.mpl.testing.MPLTestBase
+
+class TestTest extends MPLTestBase {
+
+}
