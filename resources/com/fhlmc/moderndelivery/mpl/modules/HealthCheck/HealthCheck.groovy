0eeb6473751e116297b9ef15137510a0069f01a5
sasi kumar anandan <skumar@agiletrailblazers.com>
2019-12-24 13:29:57 +0000

    removed artifactory HC HealthCheck.groovy edited online with Bitbucket

--- resources/com/fhlmc/moderndelivery/mpl/modules/HealthCheck/HealthCheck.groovy
+++ resources/com/fhlmc/moderndelivery/mpl/modules/HealthCheck/HealthCheck.groovy
@@ -17,7 +17,7 @@ echo 'Toolchain Healthcheck ...'
 
 MPLModule('Jira HealthCheck', CFG)
 MPLModule('Sonar HealthCheck', CFG)
-MPLModule('Artifactory HealthCheck', CFG)
+//MPLModule('Artifactory HealthCheck', CFG)
 //MPLModule('Elasticache HealthCheck', CFG)
 //MPLModule('Fluentd HealthCheck', CFG)
 //MPLModule('Kibana HealthCheck', CFG)
