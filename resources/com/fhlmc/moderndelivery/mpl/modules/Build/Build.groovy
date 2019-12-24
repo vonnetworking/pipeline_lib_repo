9f391a343b83a169d8b6bfeca5081b57f07878e6
sasi kumar anandan <skumar@agiletrailblazers.com>
2019-12-24 13:02:38 +0000

    Added check to find if buil.gradle exists Build.groovy edited online with Bitbucket

--- resources/com/fhlmc/moderndelivery/mpl/modules/Build/Build.groovy
+++ resources/com/fhlmc/moderndelivery/mpl/modules/Build/Build.groovy
@@ -5,7 +5,9 @@
 
 def gradle = 'build'
 
-if(CFG.'build_tool_gradle') {
+//if(CFG.'build_tool_gradle') {
+def exists = fileExists 'build.gradle'
+ if (exists) {
   MPLModule('Gradle Build', CFG)
 } 
 
